e553d3b13e29a4d078b624f4ec6f8cf32e82cc0c
marcinoles87 <marcinoles87@gmail.com>
2024-02-20 21:56:47 +0100

    add nav

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ function App() {
   return (
     <div className="App">
       <Navig></Navig>
+      <div className='image'>
+        <div className='image-overlay'></div>
+      </div>
+      
       <Main></Main>
       <Footer></Footer>
       
--- src/components/Navig.js
+++ src/components/Navig.js
@@ -1,51 +1,27 @@
 import React from 'react'
-import '../App.css'
+// import '../App.css'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 function Navig() {
   return (
-    <div className='nav-container'>
-      
+  
           <Navbar bg="dark" data-bs-theme="dark">
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-      <br />
-      <Navbar bg="primary" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#home">About</Nav.Link>
+            <Nav.Link href="#features">Tockenize</Nav.Link>
+            <Nav.Link href="#pricing">How to Buy</Nav.Link>
+            <Nav.Link href="#pricing">Telegram</Nav.Link>
+            <Nav.Link href="#pricing">Chart</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
 
-      <br />
-      <Navbar bg="light" data-bs-theme="light">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-    
   
-
-    </div>
   )
 }
 
-export default Nav
+export default Navig
