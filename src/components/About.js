3890c221bd17c9a01498d668c5e813bdd84633c7
marcinoles87 <marcinoles87@gmail.com>
2024-02-21 10:11:03 +0100

    add about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import logo from '../img/logo_img.png'
 
 function About() {
   return (
     <div className='about-container'>
-        <img src='' alt='about-log'></img>
+        <img src={logo} alt='about-log'></img>
         <div className='about-info'>
             <h1>About</h1>
             <p>If you don't believe it or don't get it, I don't have the time to try to convince you, sorry.
