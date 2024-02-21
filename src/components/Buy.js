e377f4e8b7b91fc7cb7fd3562309d783fd98e51a
marcinoles87 <marcinoles87@gmail.com>
2024-02-21 12:25:28 +0100

    add about section

---
+++ src/components/Buy.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import '../App.css'
+
+function Buy() {
+  return (
+    <div className='buy-container'>
+        <h1>How to buy</h1>
+        <div className='buy-item'>
+            <h1>Make Wallet</h1>
+            <p>download Phantom or your wallet of choice from the app store or google play store for free. Desktop users, download the google chrome extension by going to Phantom</p>
+        </div>
+
+        <div className='buy-item'>
+            <h1>Make Wallet</h1>
+            <p>download Phantom or your wallet of choice from the app store or google play store for free. Desktop users, download the google chrome extension by going to Phantom</p>
+        </div>
+
+        <div className='buy-item'>
+            <h1>Make Wallet</h1>
+            <p>download Phantom or your wallet of choice from the app store or google play store for free. Desktop users, download the google chrome extension by going to Phantom</p>
+        </div>
+
+        <div className='buy-item'>
+            <h1>Make Wallet</h1>
+            <p>download Phantom or your wallet of choice from the app store or google play store for free. Desktop users, download the google chrome extension by going to Phantom</p>
+        </div>
+
+    </div>
+  )
+}
+
+export default Buy
