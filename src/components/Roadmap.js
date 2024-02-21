11a083fbe7fbaed5320792313930027bde423a3e
marcinoles87 <marcinoles87@gmail.com>
2024-02-21 12:58:55 +0100

    add about section

--- src/components/Roadmap.js
+++ src/components/Roadmap.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import '../App.css'
+import logo from '../img/logo_img.png'
 
 function Roadmap() {
   return (
     <div className='roadmap-container'>
         <h1>RoadMap</h1>
-        <div roadmap-info>
-            <img src='' alt='roadmap-logo'></img>
-            <p>Phase 1: The People's Bitcoin
-                Phase 2: Vibe and HODL
+        <div className='roadmap-info'>
+            <img src={logo} alt='roadmap-logo'></img>
+            <div className='roadmap-info2'>
+            <p>Phase 1: The People's Bitcoin<br/>
+                Phase 2: Vibe and HODL<br/>
                 Phase 3: Flip Bitcoin</p>
+
+                </div>
         </div>
     </div>
   )
