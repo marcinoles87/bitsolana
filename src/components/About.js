6f3c38cc6886ebffe0044446857d494018165b45
marcinoles87 <marcinoles87@gmail.com>
2024-02-21 17:07:56 +0100

    work with css

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ import logo from '../img/logo_img.png'
 function About() {
   return (
     <div className='about-container'>
-        <img src={logo} alt='about-log'></img>
+        <img src={logo} alt='about-log' className='about_img'></img>
         <div className='about-info'>
             <h1>About</h1>
             <p>If you don't believe it or don't get it, I don't have the time to try to convince you, sorry.
