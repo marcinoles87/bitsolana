d1326af56a043afd4757527b9bf4151dbd1b5f63
marcinoles87 <marcinoles87@gmail.com>
2024-02-21 09:29:29 +0100

    add nav

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,6 +7,15 @@ function Main() {
       <div className='main-info'>
         <h1>BITCOIN</h1>
         <h2> Meet the People's Bitcoin on Solana. Bitcoin, the world's leading cryptocurrency, is now available on the rising Solana blockchain.</h2>
+        <div className='main-links'>
+            <i class="fa-brands fa-twitter fa-2xl"></i>
+            <i class="fa-brands fa-telegram fa-2xl"></i>
+        </div>
+        <div className='main-buttons'>
+          <button className='main-btn'>Buy Now</button>
+          <button className='main-btn2'>Dexscreener</button>
+        </div>
+    
       </div>
 
       <div className='main-logo'>
