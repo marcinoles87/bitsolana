2e684ca480744316647a37cf58442daa591cae5f
marcinoles87 <marcinoles87@gmail.com>
2024-02-23 08:33:55 +0100

    work with css

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import '../App.css'
+import contactLogo from '../img/contact.png'
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div className='contact-container'>
+      <h1>Contact</h1>
+      <img src={contactLogo} alt='contact-logo'></img>
+      <div className='contact-link'>
+        <i class="fa-brands fa-twitter fa-2xl"></i>
+        <i class="fa-brands fa-telegram fa-2xl"></i>
+        <i class="fa-brands fa-twitter fa-2xl"></i>
+        <i class="fa-brands fa-telegram fa-2xl"></i>
+      </div>
+      
+      <p>The People's Bitcoin coin has no association with Bitcoin of Satoshi's. This token is simply paying homage to a meme we all love and recognize.</p>
+        <p>The People's Bitcoin is a meme coin with no intrinsic value or expectation of financial return. There is no formal team or roadmap. the coin is completely useless and for entertainment purposes only.</p>
+    </div>
   )
 }
 
